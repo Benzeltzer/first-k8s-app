12cd6cb919f891d26f021c661efeb8a2ccfe12b4
benzeltz <benzeltz@ben.myguest.virtualbox.org>
2024-03-02 18:53:35 +0200

    basic service and pod in terraform file

---
+++ nginx_pod_service.tf
@@ -0,0 +1,59 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"  # Adjust the path to your kubeconfig file
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx-deployment"
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          image = "mynginx:1.0"
+          name  = "nginx-container"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-service"
+  }
+
+  spec {
+    selector = {
+      app = kubernetes_deployment.nginx.metadata[0].labels.app
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "NodePort"
+  }
+}
